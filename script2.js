473fc9b5b72a7076c5600920cf4e9f6205b20f26
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-05-26 13:37:46 -0400

    funcion validar

--- script2.js
+++ script2.js
@@ -1,2 +1,10 @@
 //validar nombre, debe contener solo letras y no ser vacio
+personas = []
+
+function validar(){
+    let eNombre = document.getElementById('nombre')
+    let eEdad = document.getElementById('edad')
+}
+
+
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
