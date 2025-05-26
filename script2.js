c34a31ccdecc622253946cdd246ad86d2c0c5089
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-05-26 14:09:38 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,43 +7,49 @@ let vEdad = eEdad.value
 let errorNombre = document.getElementById('errorNombre')
 let errorEdad = document.getElementById('errorEdad')
 
-function validarFormulario(){
+function validarFormulario() {
     let eNombre = document.getElementById('nombre')
     let eEdad = document.getElementById('edad')
     let vNombre = eNombre.value
     let vEdad = eEdad.value
     let errorNombre = document.getElementById('errorNombre')
     let errorEdad = document.getElementById('errorEdad')
-    let fNombre = validarNombre(eNombre,vNombre,errorNombre)
-    let fEdad = validarEdad(eEdad,vEdad,errorEdad)
-    if (fNombre && fEdad){
-        let persona ={
-        nombre: vNombre,
-        edad: vEdad
+    let fNombre = validarNombre(eNombre, vNombre, errorNombre)
+    let fEdad = validarEdad(eEdad, vEdad, errorEdad)
+    if (fNombre && fEdad) {
+        let persona = {
+            nombre: vNombre,
+            edad: vEdad
         }
         let personas = personas.push(persona)
         console.log(persona)
-        return 
+        return
     }
-      
-}
-
-function validarNombre(){
 
 }
 
-
-function actualizarMiFormulario(){
+function validarNombre() {
 
 }
 
-function eliminar(){
+
+function actualizarMiFormulario() {
+
+}
+
+function eliminar() {
 
 }
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
 
 
-function validarEdad(){
-
+function validarEdad() {
+    if (vEdad === '') {
+        alert("Por favor ingresar edad valida")
+        return
+    } if (vEdad < 0) {
+        alert("Edad invalida")
+        return
+    } else (vEdad != 0)
 
 }
